642809947205cd50be708e2900d7289e21197636
igor.kpl <lov3catch@gmail.com>
2018-12-19 21:15:12 +0200

    complete partner button

--- handlers/partners.py
+++ handlers/partners.py
@@ -1,8 +1,36 @@
-from menu import partners_menu
+import os
+
+from handlers.bank import get_balance
+from menu.helpers import get_chat_id
 
 
 def partners(bot, update):
-    reply_markup = partners_menu.menu(bot, update)
+    from orm.area import Area
+    from orm.channel import Channel
 
-    message = 'Посетите сайт наших партнёров:'
-    update.message.reply_text(message, reply_markup=reply_markup)
+    area = Area.get_by_id(2)
+
+    channel_id = get_chat_id(update)
+
+    referral_link = os.getenv('REFERRAL_LINK_PATTERN')
+    current_channel = Channel.get(Channel.area == area, Channel.channel_id == channel_id)
+
+    balance = get_balance(bot, update)
+    referrals = Channel.select().where(Channel.partner == current_channel)
+
+    message = """
+    Приглашайте партнёров в бот и получайте за них деньги!
+
+Отправьте другу ссылку
+{}
+
+10$ за каждого приглашенного Вами партнёра
+10% от заработка ваших друзей
+
+Приглашено пользователей: {}
+Заработок: {}$""".format(
+        referral_link.format(get_chat_id(update)),
+        len(referrals),
+        float(balance)
+    )
+    update.message.reply_text(message)
