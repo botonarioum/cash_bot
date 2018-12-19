d9dbe5255b4770d3148238c016a4e535e768efda
igor.kpl <lov3catch@gmail.com>
2018-12-19 21:17:58 +0200

    fix help

--- handlers/help.py
+++ handlers/help.py
@@ -1,5 +1,8 @@
-from handlers.start import info
+from handlers.start import start_text
+from menu import default_menu
 
 
 def help(bot, update):
-    info(bot, update)
+    reply_markup = default_menu.menu(bot, update)
+    # Reply to the message
+    update.message.reply_text(start_text, reply_markup=reply_markup)
